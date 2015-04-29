ddac9a3e68a5fba84e8ca9b8651b1c7c51f8db77
김유정 <saramary21@gmail.com>
2015-04-29 17:21:48 +0900

    first commit!

---
+++ src/person/PersonTest.java
@@ -0,0 +1,13 @@
+package person;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+//		Person p = new Person();
+//		p.age = 10;
+//		p.height = 180;
+		
+		Person student = new Student();       
+	}
+
+}
